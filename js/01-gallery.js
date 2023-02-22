f246b480c47f46a86d970104461939e55ef4e429
K-Basket <koshykkv@gmail.com>
2023-02-22 03:05:16 +0200

    gallery and lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryRef.addEventListener('click', onOpensLargeImgClick);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join('');
+}
+
+function onOpensLargeImgClick(evt) {
+  evt.preventDefault(); // off link
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImg = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${evt.target.dataset.source}">
+	`,
+  );
+
+  largeImg.show();
+
+  galleryRef.addEventListener('keydown', evt => {
+    if (evt.code !== 'Escape') {
+      return;
+    }
+
+    largeImg.close();
+  });
+}
